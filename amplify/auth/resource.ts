6f0b8aa7789e4c304784ca6f123f3f2913efb344
fujit <fujit@example.com>
2026-01-28 22:19:23 +0900

    fix: disable Google OAuth temporarily - enable email auth only

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,35 +1,41 @@
-import { defineAuth, secret } from "@aws-amplify/backend";
+import { defineAuth } from "@aws-amplify/backend";
 
 /**
  * 認証設定
- * 日本市場向け設定 + Google OAuth
+ * 日本市場向け設定
+ * 
+ * Google OAuthを有効にするには:
+ * 1. Google Cloud ConsoleでOAuth認証情報を作成
+ * 2. npx ampx sandbox secret set GOOGLE_CLIENT_ID
+ * 3. npx ampx sandbox secret set GOOGLE_CLIENT_SECRET
+ * 4. このファイルでexternalProvidersのコメントを解除
  *
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
 export const auth = defineAuth({
   loginWith: {
     email: true,
-    // Google Social Login
-    externalProviders: {
-      google: {
-        clientId: secret("GOOGLE_CLIENT_ID"),
-        clientSecret: secret("GOOGLE_CLIENT_SECRET"),
-        scopes: ["email", "profile", "openid"],
-        attributeMapping: {
-          email: "email",
-          fullname: "name",
-          profilePicture: "picture",
-        },
-      },
-      callbackUrls: [
-        "http://localhost:3000/",
-        "https://master.dkgndrbx9dc92.amplifyapp.com/",
-      ],
-      logoutUrls: [
-        "http://localhost:3000/",
-        "https://master.dkgndrbx9dc92.amplifyapp.com/",
-      ],
-    },
+    // Google Social Login - 有効化するには以下のコメントを解除
+    // externalProviders: {
+    //   google: {
+    //     clientId: secret("GOOGLE_CLIENT_ID"),
+    //     clientSecret: secret("GOOGLE_CLIENT_SECRET"),
+    //     scopes: ["email", "profile", "openid"],
+    //     attributeMapping: {
+    //       email: "email",
+    //       fullname: "name",
+    //       profilePicture: "picture",
+    //     },
+    //   },
+    //   callbackUrls: [
+    //     "http://localhost:3000/",
+    //     "https://master.dkgndrbx9dc92.amplifyapp.com/",
+    //   ],
+    //   logoutUrls: [
+    //     "http://localhost:3000/",
+    //     "https://master.dkgndrbx9dc92.amplifyapp.com/",
+    //   ],
+    // },
   },
   // ユーザー属性
   userAttributes: {
